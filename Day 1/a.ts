70e67e115b384729731859f2e29611dc936bbc35
Kevin Tatooles <kevintatooles@gmail.com>
2025-12-02 22:38:58 -0600

    Clean up logic using modulo

--- Day 1/a.ts
+++ Day 1/a.ts
@@ -10,25 +10,16 @@ const moveDial = (direction: string, distance: number) => {
     dial += distance;
   }
 
-  while (dial < 0 || dial >= 100) {
-    if (dial < 0) {
-      dial += 100;
-    } else if (dial >= 100) {
-      dial -= 100;
-    }
-  }
-
-  //   console.log(dial);
-  if (dial === 0) zeroCount++;
+  if (dial % 100 === 0) zeroCount++;
 };
 
-const arr = input.split(/\r?\n/).forEach((rotation) => {
+input.split(/\r?\n/).forEach((rotation) => {
   const direction = rotation.slice(0, 1);
   const distance = parseInt(rotation.slice(1));
 
   moveDial(direction, distance);
 
-  console.log({ direction, distance });
+  //   console.log({ direction, distance });
 });
 
 console.log(zeroCount);
